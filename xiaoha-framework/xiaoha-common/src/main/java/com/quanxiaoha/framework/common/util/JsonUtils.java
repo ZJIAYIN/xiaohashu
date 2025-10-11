5963107a4c7caf1b17583543b2d562f993d85225
ZJIAYIN <1396433750@qq.com>
2025-10-11 22:03:43 +0800

    添加全局异常捕获，接口参数校验

--- xiaoha-framework/xiaoha-common/src/main/java/com/quanxiaoha/framework/common/util/JsonUtils.java
+++ xiaoha-framework/xiaoha-common/src/main/java/com/quanxiaoha/framework/common/util/JsonUtils.java
@@ -5,15 +5,22 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.quanxiaoha.framework.common.constant.DateConstants;
 import lombok.SneakyThrows;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class JsonUtils {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
         OBJECT_MAPPER.registerModules(new JavaTimeModule()); // 解决 LocalDateTime 的序列化问题
     }
 
@@ -25,6 +32,15 @@ public class JsonUtils {
     @SneakyThrows
     public static String toJsonString(Object obj) {
        return OBJECT_MAPPER.writeValueAsString(obj);
+    }
+
+    /**
+     * 初始化：统一使用 Spring Boot 个性化配置的 ObjectMapper
+     *
+     * @param objectMapper
+     */
+    public static void init(ObjectMapper objectMapper) {
+        OBJECT_MAPPER = objectMapper;
     }
 
 }
--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.quanxiaoha.xiaohashu.auth.controller;
 
+import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.quanxiaoha.framework.common.response.Response;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,8 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
+    @ApiOperationLog(description = "测试接口")
     public Response<String> test() {
         return Response.success("Hello, 犬小哈专栏");
     }
+
+    @PostMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2(@RequestBody @Validated User user) {
+        //int i = 1/0;
+        return Response.success(user);
+    }
+
 }
 
