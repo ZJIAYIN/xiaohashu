abbd1cb05086ed82316fe0572ac26c68571fb05f
ZJIAYIN <1396433750@qq.com>
2025-10-15 19:24:31 +0800

    引入了nacos的依赖

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/runner/PushRolePermissions2RedisRunner.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/runner/PushRolePermissions2RedisRunner.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
